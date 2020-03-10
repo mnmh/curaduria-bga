9d1c56f44a3325220367556c1cede381c0021996
Laura Junco <lc.junco10@uniandes.edu.co>
2020-03-10 15:08:22 -0500

    Crear menu de obras

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,7 @@
 (function ($, root, undefined) {
 
+  var a = document.getElementsByTagName("path")[0]; //selecciona el tag objeto
+  var length = a.getTotalLength();
+  console.log("length", length);
 
 })(jQuery, this);
