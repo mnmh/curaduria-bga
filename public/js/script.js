9cbec83f87daa7e1377edbd783719ada70cc8ce4
Laura Junco <lc.junco10@uniandes.edu.co>
2020-03-17 11:04:57 -0500

    Add tween to animate svg

--- public/js/script.js
+++ public/js/script.js
@@ -5,11 +5,29 @@
   //console.log("length", length);
 
   /* ---------------------------------------------
+  ANIMATE SVG
+  ------------------------------------------------*/
+	function pathPrepare ($el) {
+		var lineLength = $el[0].getTotalLength();
+		$el.css("stroke-dasharray", lineLength);
+		$el.css("stroke-dashoffset", lineLength);
+  }
+  
+  var $path = $("path#path");
+  pathPrepare($path);
+
+  var tl = gsap.timeline();
+  tl.to($path, {duration: 5, strokeDashoffset: 0, ease:Linear.easeNone });
+
+
+	
+  /* ---------------------------------------------
   CONTROLES DE AUDIO
   ------------------------------------------------*/
-  document.getElementById("toggle-play").addEventListener("click", togglePlay);
+  //$("#1").on("scroll", console.log('scroll'));
+  $("#toggle-play").click(togglePlay);
 
-  var x = document.getElementById("audio"); 
+  var x = $("#audio")[0]; 
 
   function togglePlay() { 
     if (x.currentTime === 0 || (x.paused && x.currentTime > 0 && !x.ended)){
