2bbca2937efea0b6486d126c98dd16a52733ddaf
Laura Junco <lc.junco10@uniandes.edu.co>
2020-03-17 09:36:49 -0500

    Agregar botón para reproducit/pausar audi

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,24 @@
 (function ($, root, undefined) {
 
-  var a = document.getElementsByClassName("ensayo")[0]; //selecciona el tag objeto
-  var length = a.getTotalLength();
+  //var a = document.getElementsByClassName("ensayo")[0]; //selecciona el tag objeto
+  //var length = a.getTotalLength();
   //console.log("length", length);
 
+  /* ---------------------------------------------
+  CONTROLES DE AUDIO
+  ------------------------------------------------*/
+  document.getElementById("toggle-play").addEventListener("click", togglePlay);
 
-  
+  var x = document.getElementById("audio"); 
+
+  function togglePlay() { 
+    if (x.currentTime === 0 || (x.paused && x.currentTime > 0 && !x.ended)){
+      x.play();
+      console.log('play');
+    } else {
+      x.pause();
+      console.log('pause');
+    }
+  }
 
 })(jQuery, this);
