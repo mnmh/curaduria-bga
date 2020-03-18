72b02e136b2ba9694bf8aa6c0f169d31bf8b8ac0
Laura Junco <lc.junco10@uniandes.edu.co>
2020-03-18 09:59:22 -0500

    Crear función para reproducir/parar audio

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,7 @@
 (function ($, root, undefined) {
 
   var audio = document.getElementById("audio");
+  var $boton = $("#boton-audio");
 
   var id = 1;
   var $section = $('#'+ id);
@@ -9,38 +10,43 @@
   var audioTo = $section.attr("audio-to");
   audio.currentTime = parseFloat(audioFrom);
   
-  //preparar path
+  
+  //pausar reproducir audio
+  $boton.click(playPauseAudio);
+
+  function playPauseAudio() {
+    var playing = $boton.hasClass('playing') ? true : false;
+    
+    if (!playing) {
+      $boton.removeClass('playing').removeClass('paused');
+      $boton.addClass('playing');
+
+      audio.play();
+
+      audio.ontimeupdate = () => {
+          var timeCurrent = audio.currentTime - audioFrom;
+          var timeTotal = audioTo - audioFrom;
+          updateAnimation(timeCurrent, timeTotal);
+    }
+
+    } else {
+      $boton.removeClass('playing').removeClass('paused');
+      $boton.addClass('paused');
+
+      audio.pause();
+    }
+  }
+
+})(jQuery, this);
+
+
+ /* //preparar path
   pathPrepare($path);
 
-  //pausar reproducir audio
-  $("#toggle-play").click(togglePlay);
 
   //funcion para preparar el path
 	function pathPrepare ($p) {
 		var lineLength = $p[0].getTotalLength();
 		$p.css("stroke-dasharray", lineLength);
 		$p.css("stroke-dashoffset", lineLength);
-  }
-  
-  //animación svg
-  var drawPath = gsap.to(
-    $path, 
-      { 
-      duration: (audioTo - audioFrom),
-      strokeDashoffset: 0, 
-      ease:Linear.easeNone,
-      paused:true,
-      onComplete: audio.pause()
-  });
-  
-  //funcion para Pausar / reproducir Audio
-  function togglePlay() { 
-    if (audio.currentTime === 0 || (audio.paused && audio.currentTime > 0 && !audio.ended)){
-      audio.play(); //reproducir
-      drawPath.play(); //empieza anim
-    } else {
-      audio.pause(); //pausar
-      drawPath.pause(); //pausar anim
-    }
-  }
-})(jQuery, this);
+  }*/
