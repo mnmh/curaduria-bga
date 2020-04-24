f105a01b0411b07093f4d6e4cbabfb89a1a8d9c2
Laura Junco <lc.junco10@uniandes.edu.co>
2020-04-24 10:12:06 -0500

    Agregar textos corregidos, audio, cuadrar audios, y agregar script para hacer scroll a galeria en obra auras anonimas.

--- public/js/modal.js
+++ public/js/modal.js
@@ -19,5 +19,20 @@
         return false;
     })
 
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+
+        if (this.hash !== "") {
+    
+            event.preventDefault();
+    
+            var hash = this.hash;
+    
+            $('html, body').animate({
+            scrollTop: $(hash).offset().top
+            }, 800, function(){
+                });
+        }
+        });
 
 })(jQuery, this);
